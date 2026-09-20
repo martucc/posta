c65ab6edad2f40f25f2cd12a62cc8d7bdf74c409
Nicolò Martuccio <2005niky@gmail.com>
2026-09-20 21:45:42 +0200

    Posta della sera: riepilogo serale come PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Rete per prima: il riepilogo cambia ogni sera, la cache serve solo da rete di sicurezza.
+const CACHE = 'posta-sera-v1';
+
+self.addEventListener('install', e => self.skipWaiting());
+
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+));
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(hit => hit || caches.match('./')))
+  );
+});
